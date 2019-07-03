f8c1349dd0ac55eefe93e4773591fd075290a554
shiming.li <lamshiming@sina.com>
2019-07-03 18:51:10 +0800

    init

--- app.js
+++ app.js
@@ -34,6 +34,8 @@ App({
     })
   },
   globalData: {
-    userInfo: null
+    userInfo: null,
+    name:"shiming",
+    loacolNum:110
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,12 +1,16 @@
 //index.js
 //获取应用实例
 const app = getApp()
-
+console.log("shiming app=="+app.globalData.name)
+console.log("shiming app==" + app.globalData.loacolNum)
 Page({
+  //页面的初始数据
   data: {
     motto: 'Hello World',
+    dataMsg:"文学体裁的一种，侧重于事件发展",
     userInfo: {},
     hasUserInfo: false,
+    //   //判断小程序的API，回调，参数，组件等是否在当前版本可用。
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
   //事件处理函数
@@ -15,7 +19,10 @@ Page({
       url: '../logs/logs'
     })
   },
+  //	生命周期回调—监听页面加载
   onLoad: function () {
+    console.log("shiming  app.globalData.userInfo="  + app.globalData.userInfo)
+    console.log("shiming  this.data.canIUse=" + this.data.canIUse)
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
@@ -25,6 +32,7 @@ Page({
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
+        console.log("shiming   app.userInfoReadyCallbacke=" + res.userInfo.avatarUrl)
         this.setData({
           userInfo: res.userInfo,
           hasUserInfo: true
@@ -44,7 +52,7 @@ Page({
     }
   },
   getUserInfo: function(e) {
-    console.log(e)
+    console.log("shiming"+e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
       userInfo: e.detail.userInfo,
